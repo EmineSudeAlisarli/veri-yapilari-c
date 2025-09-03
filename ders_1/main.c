5b33b78cc1fb345d1d7892d86dfcb0bef933ffc0
Emine Sude Alisarli <eminesudealisarli@gmail.com>
2025-09-03 15:33:14 +0300

    döngü yapıları ile gezme eklendi

--- ders_1/main.c
+++ ders_1/main.c
@@ -3,18 +3,18 @@
 
 struct n{
         int x;//Veriyi tutan alan
-        struct n * next;//Bir sonraki dügümün adresini tutan pointer
+        struct n * next;//Bir sonraki dÃ¼gÃ¼mÃ¼n adresini tutan pointer
 };
 typedef struct n node;// 'struct n' yerine sadece 'node' yazabilmek icin
 
 int main()
 {
-    node * root; // Bagli listenin baslangic dügümünü gösteren pointer
+    node * root; // Bagli listenin baslangic dÃ¼gÃ¼mÃ¼nÃ¼ gÃ¶steren pointer
     root = (node *)malloc(sizeof(node));//ilk kutuyu olusturduk
-    root ->x = 10; //Pointer'in gösterdigi bellek adresindeki x alanina eris
+    root ->x = 10; //Pointer'in gÃ¶sterdigi bellek adresindeki x alanina eris
 
-    root -> next = (node *)malloc(sizeof(node));//yeni bir kutu oluþtu ve bunu ilk kutuya baðladý
-    root -> next -> x = 20; //rootun gösterdiði kutunun nextinin x i
+    root -> next = (node *)malloc(sizeof(node));//yeni bir kutu oluÃ¾tu ve bunu ilk kutuya baÃ°ladÃ½
+    root -> next -> x = 20; //rootun gÃ¶sterdiÃ°i kutunun nextinin x i
 
     root -> next -> next = (node *)malloc(sizeof(node));
     root -> next -> next -> x = 30;
@@ -28,12 +28,17 @@ int main()
 
     iter = root;
     int i = 0;
-    while(iter != NULL){
+    while(iter != NULL){//listenin icinde gezme
         i++;
         printf("%d. eleman: %d\n",i, iter -> x);
         iter = iter -> next;
     }
-
+    for(i = 0;i<5;i++){//listeye yeni nodelar ekleme ve her node iÃ§in deÄŸer verme.
+        iter -> next = (node *)malloc(sizeof(node)); //yeni kutu oluusturduk
+        iter = iter -> next //artÄ±k o kutuya erisim var.
+        iter -> x = i*10;
+        iter -> next = NULL;//son kutu NULL degerine esit.
+    }
 }
 
 
